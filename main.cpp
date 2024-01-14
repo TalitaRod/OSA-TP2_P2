7cebc60aaf1f86398c8d31e9c74cc5030abdffc0
Talita Rodrigues <51852494+TalitaRod@users.noreply.github.com>
2024-01-14 17:58:11 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "Netflix.h"
+#include "Arquivos.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    Arquivos arqu;
+
+    cout<<"Trabalho Pratico 2 - Parte 2\n"<<endl;
+
+    arqu.EscritaArquivo();
+    arqu.Menu();
+
+    return 0;
+}
